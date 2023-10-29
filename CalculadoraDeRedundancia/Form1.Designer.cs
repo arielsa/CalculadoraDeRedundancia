2de9aeeba78e41aadd5895e10df0e650b2516b24
ariel <santa.cruz.unla@gmail.com>
2023-10-28 22:12:33 -0300

    Agregar archivos de proyecto.

---
+++ CalculadoraDeRedundancia/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CalculadoraDeRedundancia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_m = new System.Windows.Forms.TextBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.txt_r = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgv_cuadro = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_cuadro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tamaño de mensaje";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_m
+            // 
+            this.txt_m.Location = new System.Drawing.Point(214, 60);
+            this.txt_m.Name = "txt_m";
+            this.txt_m.Size = new System.Drawing.Size(100, 26);
+            this.txt_m.TabIndex = 1;
+            this.txt_m.TextChanged += new System.EventHandler(this.txt_m_TextChanged);
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.Location = new System.Drawing.Point(364, 62);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(104, 37);
+            this.btn_calcular.TabIndex = 2;
+            this.btn_calcular.Text = "calcular";
+            this.btn_calcular.UseVisualStyleBackColor = true;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // txt_r
+            // 
+            this.txt_r.Location = new System.Drawing.Point(214, 92);
+            this.txt_r.Name = "txt_r";
+            this.txt_r.ReadOnly = true;
+            this.txt_r.Size = new System.Drawing.Size(100, 26);
+            this.txt_r.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Redundancia";
+            // 
+            // dgv_cuadro
+            // 
+            this.dgv_cuadro.AllowUserToAddRows = false;
+            this.dgv_cuadro.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_cuadro.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_cuadro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv_cuadro.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_cuadro.Location = new System.Drawing.Point(67, 147);
+            this.dgv_cuadro.Name = "dgv_cuadro";
+            this.dgv_cuadro.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_cuadro.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgv_cuadro.RowHeadersWidth = 62;
+            this.dgv_cuadro.RowTemplate.Height = 28;
+            this.dgv_cuadro.Size = new System.Drawing.Size(608, 413);
+            this.dgv_cuadro.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 591);
+            this.Controls.Add(this.dgv_cuadro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_r);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_m);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_cuadro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_m;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.TextBox txt_r;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgv_cuadro;
+    }
+}
+
---
+++ CalculadoraDeRedundancia/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraDeRedundancia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        Tabla tabla;
+
+        private void label1_Click(object sender, EventArgs e)
+        {        }
+        private void txt_m_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public int calcularRedundancia(int m, int r)
+        {
+            double n = Math.Pow(2, r);
+            if ( !(m+1+r <= n ))
+            {
+                r++;
+                r = calcularRedundancia(m, r);
+            }
+            return r;
+        }
+        public void LlenarTabla(int m, int r, Tabla tabla)
+        {            
+            double NalaR = Math.Pow(2, r);
+            int Erre = r;
+            int MmasRmasUno = m + r + 1;
+            if (!(MmasRmasUno <= NalaR))
+            {
+                r++;
+                tabla.agregarFila(new Fila(MmasRmasUno, Erre));
+                 LlenarTabla(m, r, tabla);
+            }                  
+        }
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            tabla = new Tabla();
+            int redundancia =  calcularRedundancia(Convert.ToInt32(txt_m.Text), 0);
+            txt_r.Text = redundancia.ToString();
+            LlenarTabla(Convert.ToInt32(txt_m.Text), 0, tabla);
+            int Maux = Convert.ToInt32(txt_m.Text)+redundancia+1;
+            Fila fila = new Fila(Maux, redundancia);
+            tabla.agregarFila(fila);
+            //imprimir tabla en el datagridview
+            dgv_cuadro.DataSource = null;
+            dgv_cuadro.DataSource = tabla.filas;
+
+        }
+        public class Fila
+        {
+            public Fila()
+            {                
+            }
+            public Fila(int m, int r)
+            {
+               
+                Erre = r;
+                MmasRmasUno = m;
+                DosALaR = Math.Pow(2, r); 
+            }
+            
+            public int Erre { get; set; }
+            public int MmasRmasUno { get; set; }
+            public double DosALaR { get; set; }
+        }
+        public class Tabla
+        {
+            public Tabla()
+            {
+                filas = new List<Fila>();
+            }
+            public List<Fila> filas { get; set; }
+
+            public void agregarFila(Fila fila)
+            {
+                filas.Add(fila);
+            }
+
+        }
+    }
+}
